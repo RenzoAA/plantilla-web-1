46d08ec13e43032e302b3dd43b0f908f7abdcf5d
ralvarezrenzo@gmail.com <ralvarezrenzo@gmail.com>
2023-09-07 21:42:21 -0500

    agregando seccion de btn scroll

--- js/doom/btnScroll.js
+++ js/doom/btnScroll.js
@@ -2,6 +2,16 @@ const d = document,
 w = window;
 export default function scrollBtn(btn){
     const $scrollBtn = d.querySelector(btn);
+    w.addEventListener("scroll",e=>{
+        let scrollTop = w.pageYOffset || d.documentElement.scrollTop;
+        if(scrollTop > 600){
+            $scrollBtn.classList.remove("hidden")
+        }
+        else{
+            
+            $scrollBtn.classList.add("hidden")
+        }
+    })
     d.addEventListener("click", e =>{
         if(e.target.matches(btn)|| e.target.matches(`${btn} *`)){
             w.scrollTo({
