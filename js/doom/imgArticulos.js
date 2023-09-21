0dcc6a3ed16c81c6a8a44800623031960b0895e9
ralvarezrenzo@gmail.com <ralvarezrenzo@gmail.com>
2023-09-20 20:36:20 -0500

    agregando estilos a slider de todas las url

--- js/doom/imgArticulos.js
+++ js/doom/imgArticulos.js
@@ -1,28 +1,31 @@
 const d = document;
-export default function imgArticulos(imagenArticulo){
-    const $imgArticulo = d.querySelector(imagenArticulo)
-    console.log('hola')
+export default function imgArticulos(imagenArticulo,bannerBg){
+    const $imgArticulo = d.querySelector(imagenArticulo),
+    $bannerBg = d.querySelector(bannerBg);
+    console.log($bannerBg)
     addEventListener('resize',()=>{
-        if(innerWidth < 767) {
+        if(innerWidth < 768) {
+            $bannerBg.style.backgroundImage = "url(../imagenes/plantilla-imagenes/articulo-mobile.jpg)";
             $imgArticulo.setAttribute("src", "../imagenes/plantilla-imagenes/articulo-mobile.jpg")
-        }else if (innerWidth > 768){
-            $imgArticulo.setAttribute("src", "../imagenes/plantilla-imagenes/articulo-tablet.jpg")
-        }else if (innerWidth < 1199){
+        }else if (innerWidth > 767 | innerWidth < 1199){
+            $bannerBg.style.backgroundImage = "url(../imagenes/plantilla-imagenes/articulo-tablet.jpg)";
             $imgArticulo.setAttribute("src", "../imagenes/plantilla-imagenes/articulo-tablet.jpg")
         }
         if(innerWidth > 1200){
+            $bannerBg.style.backgroundImage = "url(../imagenes/plantilla-imagenes/articulo-desktop.jpg)";
             $imgArticulo.setAttribute("src", "../imagenes/plantilla-imagenes/articulo-desktop.jpg")
         }
     })
     addEventListener('DOMContentLoaded',()=>{
         if(innerWidth < 767) {
+            $bannerBg.style.backgroundImage = "url(../imagenes/plantilla-imagenes/articulo-mobile.jpg)";
             $imgArticulo.setAttribute("src", "../imagenes/plantilla-imagenes/articulo-mobile.jpg")
-        }else if (innerWidth > 768){
-            $imgArticulo.setAttribute("src", "../imagenes/plantilla-imagenes/articulo-tablet.jpg")
-        }else if (innerWidth < 1199){
+        }else if (innerWidth > 767 | innerWidth < 1199){
+            $bannerBg.style.backgroundImage = "url(../imagenes/plantilla-imagenes/articulo-tablet.jpg)";
             $imgArticulo.setAttribute("src", "../imagenes/plantilla-imagenes/articulo-tablet.jpg")
         }
         if(innerWidth > 1200){
+            $bannerBg.style.backgroundImage = "url(../imagenes/plantilla-imagenes/articulo-desktop.jpg)";
             $imgArticulo.setAttribute("src", "../imagenes/plantilla-imagenes/articulo-desktop.jpg")
         }
     })
--- js/index.js
+++ js/index.js
@@ -8,5 +8,5 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     navBar('.hamburger-box','.nav-principal','.header__content');
     scrollBtn('.btn-scroll');
     mediaQuerys('.img-slider-1','.img-slider-2','.img-slider-3');
-    imgArticulos('.img-articulo')
+    imgArticulos('.img-articulo','.banner-nosotros')
 })
